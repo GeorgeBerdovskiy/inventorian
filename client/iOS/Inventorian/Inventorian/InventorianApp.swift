5c271d3ed8f48fd8e4d1603c9cde38ab28458dad
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2022-04-14 21:04:44 -0700

    Create iOS Application (Xcode)

---
+++ client/iOS/Inventorian/Inventorian/InventorianApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventorianApp.swift
+//  Inventorian
+//
+//  Created by George Berdovskiy on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct InventorianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
